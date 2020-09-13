93983d63c186bca4d6053401fa7e4f1330a73f00
PNKamau <patrick.ngotho@gmail.com>
2020-09-13 23:58:06 +0300

    Second update with the output file included.

--- Scripts/run_analysis.R
+++ Scripts/run_analysis.R
@@ -3,8 +3,11 @@ library(stringr)
 library(dplyr)
 library(tidyr)
 
+#Clear the R environment before running script
+rm(list = ls())
+
 #Set working directory
-setwd("D:\\R_Training/week4project/Dataset")
+setwd("D:\\R_Training/getting_and_cleaning_data/getting_and_cleaning_data2")
 
 #Set up variable for columns to extract on test data
 thewidths <- c(17)
@@ -18,13 +21,13 @@ features$V2 <- gsub(',', '_', features$V2) #Remove the "," from the labels: thes
 activity <- read.delim("activity_labels.txt", header = FALSE, sep = " ")
 colnames(activity) <- c("activity_code", "activity_desc")
 
-subject_test <- read.delim("./test/subject_test.txt", header = FALSE)
-y_test <- read.delim("./test/y_test.txt", header = FALSE)
-x_test <- read.fwf("./test/X_test.txt", widths = thewidths, header = FALSE)
+subject_test <- read.delim("./Data/test/subject_test.txt", header = FALSE)
+y_test <- read.delim("./Data/test/y_test.txt", header = FALSE)
+x_test <- read.fwf("./Data/test/X_test.txt", widths = thewidths, header = FALSE)
 
-subject_train <- read.delim("./train/subject_train.txt", header = FALSE)
-y_train <- read.delim("./train/y_train.txt", header = FALSE)
-x_train <- read.fwf("./train/X_train.txt", widths = thewidths, header = FALSE)
+subject_train <- read.delim("./Data/train/subject_train.txt", header = FALSE)
+y_train <- read.delim("./Data/train/y_train.txt", header = FALSE)
+x_train <- read.fwf("./Data/train/X_train.txt", widths = thewidths, header = FALSE)
 
 #Tidy the test data
 colnames(x_test) <- features$V2
@@ -55,3 +58,6 @@ merged_data_std_mean <- merged_data %>%
 merged_data_std_mean_summarized <- merged_data_std_mean %>%
   group_by(activity_desc, subject_code) %>%
   summarize_if(is.numeric, mean)
+
+#Export the data summarized data to a csv file
+write.csv(merged_data_std_mean_summarized,"./Output/final_output.csv", row.names = FALSE)
